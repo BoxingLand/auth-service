0047d7d19eb8bd771f5e294240f8ae155c75008f
DanilEmurashin <emurashin@gmail.com>
2024-01-31 22:58:02 +0500

    refactor logging

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ import asyncio
 import logging
 
 import grpc
+from loguru import logger
 
 from app.auth import auth_pb2_grpc
 from app.auth.auth_server import Auth
@@ -15,7 +16,7 @@ async def serve() -> None:
     auth_pb2_grpc.add_AuthServicer_to_server(Auth(), server)
     listen_addr = f"[::]:{settings.SERVER_PORT}"
     server.add_insecure_port(listen_addr)
-    logging.info("Starting server on %s", listen_addr)
+    logger.info(f"Starting server on {listen_addr}")
     await server.start()
     await server.wait_for_termination()
 
